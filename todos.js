4338623841a44292c8b9b68fe9713fe2e9a9a6e5
alberto.espinal.c@gmail.com <alberto.espinal.c@gmail.com>
2015-08-23 14:42:49 -0400

    Adding the ui for the todolist

--- todos.js
+++ todos.js
@@ -1,6 +1,11 @@
 Todos = new Mongo.Collection('todos');
 if (Meteor.isClient) {
-
+	//Template Helpers
+	Template.main.helpers({
+		todos: function(){
+			return Todos.find();
+		}
+	});
 }
 
 if (Meteor.isServer) {
