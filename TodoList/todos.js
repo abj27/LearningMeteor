9d3015975577c65f76ff1eaf04ed3e708ec06395
alberto.espinal.c@gmail.com <alberto.espinal.c@gmail.com>
2015-08-23 16:34:16 -0400

    Removing the unsecure package and changing the dadatabase calls to methods

--- TodoList/todos.js
+++ TodoList/todos.js
@@ -4,25 +4,25 @@ if (Meteor.isClient) {
 	Template.main.helpers({
 		todos: function(){
 			return Todos.find({}, {sort: {createdAt:-1 }});
+		},
+	    currentUser: function(){
+			return Meteor.user();
 		}
 	});
 
 	Template.main.events({
 		"submit .new-todo": function(event){
 			var text = event.target.text.value; 
-			Todos.insert({
-				text: text,
-				createdAt: new Date()
-			});
-			event.target.text.value = '';
+					event.target.text.value = '';
+			Meteor.call('addTodo', text);
 			return false;
 		},
 		"click .toggle-checked": function(){
-			Todos.update(this._id, {$set:{checked: !this.checked}});
+			Meteor.call('setChecked',this._id, !this.checked);
 		},
 		"click .delete-todo": function(){
 			if(confirm("Are you sure?")){
-				Todos.remove(this._id);
+				Meteor.call("deleteTodo", this._id);
 			}
 		}
 	});
@@ -31,3 +31,28 @@ if (Meteor.isClient) {
 	});
 }
 
+Meteor.methods({
+	addTodo:function(text){
+		if(!Meteor.userId()){
+			throw new Meteor.Error("not-authorized");
+		}
+		Todos.insert({
+			text: text,
+			createdAt: new Date(),
+			userId: Meteor.userId(),
+			username: Meteor.user().username
+		});
+	},
+	deleteTodo:function(todoId){
+		if(!Meteor.userId()){
+			throw new Meteor.Error("not-authorized");
+		}
+		Todos.remove(todoId);
+	},
+	setChecked: function(todoId, setChecked){
+		if(!Meteor.userId()){
+			throw new Meteor.Error("not-authorized");
+		}
+		Todos.update(todoId, {$set:{checked: setChecked}});
+	}
+});
