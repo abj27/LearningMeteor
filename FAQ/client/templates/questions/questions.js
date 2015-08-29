9c7a0cbdc6b34a894ef102d2daef845947cddbc2
alberto.espinal.c@gmail.com <alberto.espinal.c@gmail.com>
2015-08-29 09:50:20 -0400

    Adding the quesions collection

---
+++ FAQ/client/templates/questions/questions.js
@@ -0,0 +1,10 @@
+Template.questions.helpers({
+	questions: function(){
+		return Questions.find({}).map(
+			function(question,index){
+				question.i = index;
+				return question;
+			}
+		);
+	}
+});
