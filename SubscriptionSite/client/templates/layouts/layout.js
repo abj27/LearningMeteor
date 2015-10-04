e0ce5c159dda15925e6ae0eca63a174b2ebd3ee6
Alberto Espinal <alberto.espinal.c@gmail.com>
2015-10-04 15:36:01 -0400

    Adding View plans functionality for admins

--- SubscriptionSite/client/templates/layouts/layout.js
+++ SubscriptionSite/client/templates/layouts/layout.js
@@ -1,3 +1,6 @@
 Template.registerHelper("isCurrentRoute",function(route){
 	return Router.current().route.getName() === route;
 });
+Template.registerHelper("isInRole",function(role){
+    return Meteor.user() && Meteor.user().roles.indexOf("admin") >=0;
+});
--- SubscriptionSite/lib/router.js
+++ SubscriptionSite/lib/router.js
@@ -5,6 +5,33 @@ Router.configure({
 Router.map(function(){
 	this.route("plans",{
 		path:"/",
-		template:"plans"
+		template:"plans",
+	});
+	this.route("listplans",{
+		path:"/admin/plans",
+		template:"listplans",
+		data: function(){
+			templateData = {
+				plans: Plans.find()
+			};
+			return templateData;
+		}
+	});
+
+	this.route("addplan",{
+		path:"/admin/plans/add",
+		template:"addplan",
+	});
+
+	this.route("edit",{
+		path:"/admin/plans/edit/:_id",
+		template:"editplan"
+	});
+
+	this.route("listsubscribers",{
+		path:"/admin/Subscribers",
+		template:"listsubscribers"
 	});
 });
+
+
